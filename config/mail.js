db69ec64b64b103d908c2f1443ab88b3bb42ed3a
Soham Bhowmick <bhowmicksoham2001@gmail.com>
2020-07-26 13:04:18 +0530

    Fixed mailing

--- config/mail.js
+++ config/mail.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer')
 const keys = require('./keys')
 
 transporter = nodemailer.createTransport({
-    service : 'gmail',
+    host : 'smtp.zoho.com',
+    port : 465,
+    secure : true,
     auth : {
         user : keys.email.user,
         pass : keys.email.password
